e6ca636f6097a2da9445430f3b9e958aa0e8abb3
HoSHIZA <hoshiza78@gmail.com>
2024-08-29 17:28:12 +0300

    Update InvokeTaskData.cs

--- Code/Runtime/Invoke/InvokeTaskData.cs
+++ Code/Runtime/Invoke/InvokeTaskData.cs
@@ -6,35 +6,36 @@ namespace NiGames.Scheduling.Tasks.Invoke
 {
     internal readonly struct InvokeTaskData
     {
-        public readonly Action Callback;
-        public readonly Action OnComplete;
-        public readonly Action OnStart;
-        public readonly Action OnStartDelayed;
         public readonly CancellationToken CancellationToken;
         
-        public bool IsCallbackValid => Callback != null;
+        private readonly Action _callback;
+        private readonly Action _onComplete;
+        private readonly Action _onStart;
+        private readonly Action _onStartDelayed;
+        
+        public bool IsCallbackValid => _callback != null;
         public bool IsCancellationRequested => CancellationToken.IsCancellationRequested;
         
         public InvokeTaskData(Action callback, Action onComplete, Action onStart, Action onStartDelayed, CancellationToken cancellationToken) 
             : this()
         {
-            Callback = callback;
-            OnComplete = onComplete;
-            OnStart = onStart;
-            OnStartDelayed = onStartDelayed;
+            _callback = callback;
+            _onComplete = onComplete;
+            _onStart = onStart;
+            _onStartDelayed = onStartDelayed;
             CancellationToken = cancellationToken;
         }
         
         [MethodImpl(256)]
-        public void InvokeCallback() => Callback.Invoke();
+        public void InvokeCallback() => _callback.Invoke();
         
         [MethodImpl(256)]
-        public void InvokeOnStart() => OnStart?.Invoke();
+        public void InvokeOnStart() => _onStart?.Invoke();
         
         [MethodImpl(256)]
-        public void InvokeOnStartDelayed() => OnStartDelayed?.Invoke();
+        public void InvokeOnStartDelayed() => _onStartDelayed?.Invoke();
         
         [MethodImpl(256)]
-        public void InvokeOnComplete() => OnComplete?.Invoke();
+        public void InvokeOnComplete() => _onComplete?.Invoke();
     }
 }
